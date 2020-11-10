cd2453d85e7f85df525909ac4098126252f51ca2
Hugo <>
2020-11-10 12:41:42 +0800

    :white_check_mark: Update mocked NetworkCondition

--- test/index.spec.js
+++ test/index.spec.js
@@ -18,18 +18,18 @@ describe('isSlowNetwork', function () {
         effectiveType: '4g',
         downlink: 10,
         saveData: true,
-        rtt: 20
+        rtt: 0,
+        online: true
       }
 
       const result = isSlowNetwork(networkDefinition)(mockNetworkCondition)
 
       expect(result).to.eql(true)
-
     })
   })
 
-  describe('if the downlink of network is less than definition', function(){
-    it('should return true', function(){
+  describe('if the downlink of network is less than definition', function () {
+    it('should return true', function () {
       const networkDefinition = {
         effectiveType: '2g',
         downlink: 10,
@@ -41,18 +41,18 @@ describe('isSlowNetwork', function () {
         effectiveType: '4g',
         downlink: 5,
         saveData: false,
-        rtt: 20
+        rtt: 20,
+        online: true
       }
 
       const result = isSlowNetwork(networkDefinition)(mockNetworkCondition)
 
       expect(result).to.eql(true)
-
     })
   })
 
-  describe('if the effectiveType of network is slower than definition', function(){
-    it('should return true', function(){
+  describe('if the effectiveType of network is slower than definition', function () {
+    it('should return true', function () {
       const networkDefinition = {
         effectiveType: '3g',
         downlink: 10,
@@ -64,13 +64,13 @@ describe('isSlowNetwork', function () {
         effectiveType: '2g',
         downlink: 20,
         saveData: false,
-        rtt: 20
+        rtt: 20,
+        online: true
       }
 
       const result = isSlowNetwork(networkDefinition)(mockNetworkCondition)
 
       expect(result).to.eql(true)
-
     })
   })
 })
